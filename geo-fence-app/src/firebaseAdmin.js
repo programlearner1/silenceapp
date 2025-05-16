5fcf071a837c1bd880fffb54c172879d3f4b2231
Your Name <113779810+programlearner1@users.noreply.github.com>
2025-05-16 20:33:40 +0530

    removed the backend

--- geo-fence-app/src/firebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
-
-// Initialize Firebase Admin SDK
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-module.exports = admin;
