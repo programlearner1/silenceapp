7e228d7092d622bc51988bba616e027521293490
Your Name <113779810+programlearner1@users.noreply.github.com>
2025-05-16 22:39:59 +0530

    added a new file

---
+++ geo-fence-app/src/react-app-env.d.ts
@@ -0,0 +1,23 @@
+/// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    REACT_APP_FIREBASE_API_KEY: string;
+    REACT_APP_FIREBASE_AUTH_DOMAIN: string;
+    REACT_APP_FIREBASE_PROJECT_ID: string;
+    REACT_APP_FIREBASE_STORAGE_BUCKET: string;
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID: string;
+    REACT_APP_FIREBASE_APP_ID: string;
+    REACT_APP_FIREBASE_MEASUREMENT_ID: string;
+    REACT_APP_FIREBASE_VAPID_KEY: string;
+    REACT_APP_API_URL: string;
+    REACT_APP_OPENCAGE_API_KEY: string;
+    // add any other env vars you use
+  }
+}
+
+interface Process {
+  env: NodeJS.ProcessEnv;
+}
+
+declare var process: Process; 
